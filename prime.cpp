dd696f8c386650c5b631bd170fcbd513e62a40cc
Ankita Das <daszankita23@gmail.com>
2024-08-30 14:52:59 +0530

    changes done

--- prime.cpp
+++ prime.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
+    int n,i;
     cout << "Enter the no you want to check for prime:";
     cin >> n;
-     for(i=1 ; i<)
+     for(i=2 ; i<=n-1 ; i++){
+        if(n%i==0){
+            cout << "The given no is not prime";
+            break;
+        }
+        else{
+            cout << "The given no is prime";
+        }
+     }
+     return 0;
 }
